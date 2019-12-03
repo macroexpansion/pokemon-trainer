116bd37c356b6cd832902d3d88c52103eb56382a
kemda26 <hyouka266@gmail.com>
2019-12-03 20:16:15 +0700

    train vgg

--- dataloader.py
+++ dataloader.py
@@ -1,25 +1,73 @@
-import torch
 import matplotlib.pyplot as plt
-
+import cv2
+import os
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor, ToPILImage
-from sklearn.model_selection import train_test_split
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader
 
+
+def datacounter(root='pkm/train/'):
+    classes = ['bulbasaur', 'charmander', 'jigglypuff', 'magikarp', 'mudkip', 'pikachu', 'psyduck', 'snorlax', 'squirtle']
+    X, y = [], []
+    for index, clsname in enumerate(classes):
+        imgpath = os.path.join(root, clsname)
+        for path in os.listdir(imgpath):
+            imgfile = os.path.join(imgpath, path)
+            X.append(imgfile)
+            y.append(index)
+    return X, y
+
+
+def dataplot(y_train, y_test):    
+    f, axes = plt.subplots(1, 2, figsize=(18, 6))
+    ax1, ax2 = axes
+    sns.countplot(y_train, ax=ax1)
+    ax1.set_xlabel('train')
+    ax1.set_ylabel('number')
+    ax1.set_ylim([0, 200])
+    sns.countplot(y_test, ax=ax2)
+    ax2.set_xlabel('test')
+    ax2.set_ylabel('number')
+    ax2.set_ylim([0, 200])
+    plt.show()
+
+
+def train_valid_split(dataset, valid_split_size=0.2, shuffle=True, random_seed=7):
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(valid_split_size * dataset_size))
+
+    if shuffle:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    
+    train_indices, valid_indices = indices[split:], indices[:split]
+    # print(train_indices)
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(valid_indices)
+
+    return train_sampler, len(train_indices), valid_sampler, len(valid_indices)
+
+
+def dataloader(batch_size=16, transform=ToTensor()):
+    train_data = ImageFolder(root='pkm/train/', transform=transform)
+    train_sampler, train_size, valid_sampler, valid_size = train_valid_split(train_data)
+    train_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
+    valid_loader = DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler)
+
+    test_data = ImageFolder(root='pkm/test/', transform=transform)
+    # print(test_data.targets)
+    test_loader = DataLoader(test_data, batch_size=batch_size)
+
+    return train_loader, train_size, valid_loader, valid_size, test_loader, len(test_data)
 
 if __name__ == '__main__':
-    # dataset = PokemonDataset('datasets/')
-    image_data = ImageFolder(root='datasets/', transform=ToTensor())
+    # _, y_train = dataloader('pkm/train/')
+    # _, y_test = dataloader('pkm/test/')
+    # dataplot(y_train, y_test)
 
-    X, y = [], []
-    for i in image_data:
-        tensor, label = i
-        X.append(tensor)
-        y.append(label)
-
-    print(len(X))
-
-    # x_train, x_test, y_train, y_test = train_test_split()
-    # print(data.classes) # list of classes
-    # img, target = data[0]
-    # plt.imshow(img.permute(1,2,0))
-    # plt.show()
+    pass
--- train_vgg.py
+++ train_vgg.py
@@ -1,19 +1,102 @@
-import torch
-import torchvision
+import time
+from comet_ml import Experiment
+import torch, torch.nn as nn, torch.optim as optim
+from torchvision import transforms, models
+from dataloader import dataloader
+import copy
 
-def load_dataset():
-    data_path = 'datasetes/'
-    train_dataset = torchvision.datasets.ImageFolder(
-        root=data_path,
-        transform=torchvision.transforms.ToTensor()
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=64,
-        num_workers=0,
-        shuffle=True
-    )
-    return train_loader
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    print("Using CUDA")
 
-if __name__ == '__main__':
-    print('aa')
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor()
+])
+
+train_loader, train_size, valid_loader, valid_size, test_loader, test_size = dataloader(batch_size=4, transform=transform)
+dataloader = {'train': train_loader, 'val': valid_loader}
+
+model = models.vgg16()
+model.classifier[-1] = torch.nn.Linear(in_features=4096, out_features=9)
+# print(model)
+
+if use_gpu:
+    model.cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+def train_model(model, criterion, optimizer, scheduler=None, num_epochs=100):
+    experiment = Experiment(api_key='9tplk9L0Vy6WW3K7rmdbEz7jm', project_name='pkm-trainer')
+    experiment.add_tags(['vgg16', 'no_batchnorm', 'no_pretrained'])
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    with experiment.train():
+        for epoch in range(num_epochs):
+            print("Epoch {}/{}".format(epoch, num_epochs))
+            print('-' * 10)
+
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()
+                else:
+                    model.eval()
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloader[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+                    
+                    running_loss += loss.item() #* inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+
+                    del inputs, labels, outputs, preds
+                    torch.cuda.empty_cache()
+
+                data_size = train_size if phase == 'train' else valid_size
+                epoch_loss = running_loss / data_size
+                epoch_acc = running_corrects.double() / data_size
+                if phase == 'train':
+                    experiment.log_metric('train_loss', epoch_loss, step=epoch)
+                    experiment.log_metric('train_acc', epoch_acc, step=epoch)
+                else:
+                    experiment.log_metric('val_loss', epoch_loss, step=epoch)
+                    experiment.log_metric('val_acc', epoch_acc, step=epoch)
+
+                print('{} -> Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    model.load_state_dict(best_model_wts)
+    return model
+
+model_fit = train_model(model, criterion, optimizer, num_epochs=100)
+            
