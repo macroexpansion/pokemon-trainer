bbf4a32ea88e7b1c38e494c39cd50a51949562ed
kemda26 <hyouka266@gmail.com>
2019-12-03 23:04:02 +0700

    tensorboard

--- train_vgg.py
+++ train_vgg.py
@@ -4,6 +4,7 @@ import torch, torch.nn as nn, torch.optim as optim
 from torchvision import transforms, models
 from dataloader import dataloader
 import copy
+from torch.utils.tensorboard import SummaryWriter
 
 from train_model import train_model
 
@@ -15,84 +16,84 @@ if use_gpu:
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
 ])
 
-train_loader, train_size, valid_loader, valid_size, test_loader, test_size = dataloader(colab=True, batch_size=32, transform=transform)
+train_loader, train_size, valid_loader, valid_size, test_loader, test_size = dataloader(colab=False, batch_size=32, transform=transform)
 dataloader = {'train': train_loader, 'val': valid_loader}
 
 model = models.vgg16()
 model.classifier[-1] = torch.nn.Linear(in_features=4096, out_features=9)
 # print(model)
 
-# if use_gpu:
-#     model.cuda()
+if use_gpu:
+    model.cuda()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-def train_model(model, criterion, optimizer, num_epochs=100):
-    experiment = Experiment(api_key='9tplk9L0Vy6WW3K7rmdbEz7jm', project_name='pkm-trainer')
-    experiment.add_tags(['vgg16', 'no_batchnorm', 'no_pretrained'])
+def train_model(model, criterion, optimizer, num_epochs=70):
+
+    writer = SummaryWriter(comment='--vgg16')
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
-    with experiment.train():
-        for epoch in range(num_epochs):
-            start = time.time()
-            print("Epoch {}/{}".format(epoch, num_epochs))
-            print('-' * 10)
+    for epoch in range(num_epochs):
+        start = time.time()
+        print("Epoch {}/{}".format(epoch, num_epochs))
+        print('-' * 10)
 
-            for phase in ['train', 'val']:
-                if phase == 'train':
-                    model.train()
-                else:
-                    model.eval()
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
 
-                running_loss = 0.0
-                running_corrects = 0.
+            running_loss = 0.0
+            running_corrects = 0.
 
-                for inputs, labels in dataloader[phase]:
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+            for inputs, labels in dataloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-                    optimizer.zero_grad()
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = model(inputs)
-                        _, preds = torch.max(outputs, 1)
-                        loss = criterion(outputs, labels)
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-                        if phase == 'train':
-                            loss.backward()
-                            optimizer.step()
-                    
-                    running_loss += loss.item() * inputs.size(0)
-                    running_corrects += torch.sum(preds == labels.data)
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                
+                running_loss += loss.item() #* inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-                    del inputs, labels, outputs, preds
-                    torch.cuda.empty_cache()
+                del inputs, labels, outputs, preds
+                torch.cuda.empty_cache()
 
-                data_size = train_size if phase == 'train' else valid_size
-                epoch_loss = running_loss / data_size
-                epoch_acc = running_corrects / data_size
-                # print(epoch_loss.to('cpu'), epoch_acc.to('cpu'))
-                if phase == 'train':
-                    experiment.log_metric('train_loss', epoch_loss.to('cpu'), step=epoch)
-                    experiment.log_metric('train_acc', epoch_acc.to('cpu'), step=epoch)
-                else:
-                    experiment.log_metric('val_loss', epoch_loss.to('cpu'), step=epoch)
-                    experiment.log_metric('val_acc', epoch_acc.to('cpu'), step=epoch)
+            data_size = train_size if phase == 'train' else valid_size
+            epoch_loss = running_loss / data_size
+            epoch_acc = running_corrects / data_size
 
-                print('{} -> Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-                print('\ttime', time.time() - start)
+            if phase == 'train':
+                writer.add_scalar('Loss/train', epoch_loss, epoch)
+                writer.add_scalar('Accuracy/train', epoch_acc, epoch)
+            else:
+                writer.add_scalar('Loss/test', epoch_loss, epoch)
+                writer.add_scalar('Accuracy/test', epoch_acc, epoch)
 
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
+            print('{} -> Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            print('\ttime', time.time() - start)
 
-            print()
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
